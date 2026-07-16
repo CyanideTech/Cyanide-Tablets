4a7129caed25356c943a395c2bc7e465627e2621
Cyanide <cyborg_Waffles@outlook.com>
2026-07-15 21:37:03 -0400

    Fix path for hyprland.lua and add cosmic-files

--- Install.sh
+++ Install.sh
@@ -4,8 +4,9 @@ sudo pacman -S rofi --noconfirm
 sudo pacman -S kitty --noconfirm
 sudo pacman -S waybar --noconfirm
 sudo pacman -S hyprpaper --noconfirm
+sudo pacman -S cosmic-files --noconfirm
 
-sudo rm -rf ~/.config/hyprland/hyprland.lua
+sudo rm -rf ~/.config/hypr/hyprland.lua
 cp rofi ~/.config -f -r
 cp hypr ~/.config -f -r
 cp waybar ~/.config -f -r
